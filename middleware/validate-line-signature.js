928c50d530770a3d6fab155ea954e5afab272ec7
J10022 <jacky05100815@gmail.com>
2024-12-19 16:09:44 +0800

    Update validate-line-signature.js

--- middleware/validate-line-signature.js
+++ middleware/validate-line-signature.js
@@ -4,10 +4,21 @@ import { validateSignature } from '../utils/index.js';
 const validateLineSignature = (req, res, next) => {
   const secret = config.LINE_CHANNEL_SECRET || '';
   const signature = req.header('x-line-signature');
+
+  // 新增除錯訊息
+  console.log('--- Debug Start ---');
+  console.log('LINE_CHANNEL_SECRET:', secret);
+  console.log('X-Line-Signature:', signature);
+  console.log('rawBody:', req.rawBody);
+  console.log('--- Debug End ---');
+
   if (!validateSignature(req.rawBody, secret, signature)) {
+    console.log('Signature validation failed');
     res.sendStatus(403);
     return;
   }
+
+  console.log('Signature validation succeeded, responding 200...');
   next();
 };
 
